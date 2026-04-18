7880ad5e6094f0c43e1e6545a6cf807107e6f081
Mino (Hung) <hungpikatv@gmail.com>
2026-04-18 13:30:16 +0700

    initialize

---
+++ src/main/java/com/dianxin/tori/App.java
@@ -0,0 +1,13 @@
+package com.dianxin.tori;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
